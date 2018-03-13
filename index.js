2438c2a74f760356407ce8409dc28630b6e37367
ikmarokko2 <35876458+ikmarokko2@users.noreply.github.com>
2018-03-13 08:54:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setPresence({ game: { name: `Kijkt over de prachtige server van Tytgamer06 `, type: 3 }});
+  client.user.setPresence({ game: { name: `Kijkt over de prachtige server van Tytgamer06 `, type: 4 }});
 });
 
 const prefix = "t!"
